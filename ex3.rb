35dcc4e5718c805371ca2244807177e7d872aba7
Ben Moss <ben@mossity.com>
2012-02-26 18:30:07 -0500

    Add ex3 plus extra credit at the bottom

---
+++ ex3.rb
@@ -0,0 +1,27 @@
+puts "I will now count my chickens:" # prints these words
+
+puts "Hens", 25.0 + 30 / 6
+puts "Roosters", 100.0 - 25 * 3 % 4 # prints the word and then the result of the operation
+
+puts "Now I will count the eggs:"
+
+puts 3.0 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 # prints the operation
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3.0 + 2 < 5 - 7
+
+puts "What is 3 + 2?", 3.0 + 2
+puts "What is 5 - 7?", 5.0 - 7
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater?", 5 > -2
+puts "Is it greater or equal?", 5 >= -2
+puts "Is it less or equal?", 5 <= -2
+
+# extra credit
+
+puts 3 * 2.5 # returns a floating point number
